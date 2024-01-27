a5240b6910381c785b7ffdad1027d00c7742a96a
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-26 16:28:50 -0800

    console logs to server window when requests happen

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const server = net.createServer();
 
 server.on("connection", (client) => {
   console.log("New client connected!");
+  
   client.setEncoding("utf8"); // interpret data as text
+
   client.on("data", (data) => {
+    console.log("Client is asking for: ", data);
+
     fs.readFile(`./server_files/${data}`, (err, data) => {
       if (err) {
         client.write("There was an error: ", err);
       }
+      console.log("Found the data!");
       client.write(data);
     })
   });
